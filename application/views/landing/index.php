c073171c23a66ccfe6b95b656728fcedbd18f758
Ruslan Samuel <ruslansamuel11@gmail.com>
2019-07-02 12:06:04 +0800

    Mengubah Selamat Datang

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -14,7 +14,7 @@
             <div data-thumb="<?=base_url('assets/landing/')?>images/slider-1.jpg" data-src="<?=base_url('assets/landing/')?>images/slider-1.jpg">
                 <div class="camera_caption">
                    <div class="container">
-                        <h5 class=" wow fadeInUp animated">Welcome to our</h5>
+                        <h5 class=" wow fadeInUp animated">Selamat Datang</h5>
                         <h3 class=" wow fadeInUp animated" data-wow-delay="0.5s">CLEAN, MODERN, MULTIPURPOSE THEME</h3>
                         <p class=" wow fadeInUp animated" data-wow-delay="0.8s">Our team of professionals will help you turn your dream home or flat into a reality fast. The Love Boat promises something for everyone. Now the world don't move to the beat of just one</p>
                         <a class=" wow fadeInUp animated" data-wow-delay="1s" href="#">Read More</a>
